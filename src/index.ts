b3d1df76c1ecd569ae1af570e9d332be8b1cce61
taoqf <tao_qiufeng@126.com>
2020-03-19 15:48:00 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export default function fun() {
+import { Component } from 'react';
+import { NavigationInjectedProps } from 'react-navigation';
+
+export default interface IAiMobile {
+	data: { [key: string]: unknown; };	// !!! Do not modify this
+	readonly page: Component<NavigationInjectedProps>;
+	readonly local: { [key: string]: unknown; };
+	readonly global: { [key: string]: unknown; };
+	// eslint-disable-next-line semi
+}
+
+export interface IAi extends IAiMobile {
+	emit(fd: IAiMobile, event: string, ...args: unknown[]): Promise<unknown>;
 }
