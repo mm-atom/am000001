29c5efcf753af0d2ffc2194b2feb06373dad1ff0
taoqf <tao_qiufeng@126.com>
2020-03-26 16:24:53 +0800

    navigation 5

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import { Component } from 'react';
-import { NavigationInjectedProps } from 'react-navigation';
+import { ParamListBase, RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { MaterialBottomTabNavigationProp } from '@react-navigation/material-bottom-tabs';
+import { BottomTabBarProps, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+
+type ParamList = ParamListBase;
+
+type ScreenNavigationProp = StackNavigationProp<ParamList> | DrawerNavigationProp<ParamList> | MaterialBottomTabNavigationProp<ParamList> | BottomTabNavigationProp<ParamList>;
+
+type ScreenRouteProp = RouteProp<ParamList, 'mmstudio'>;
+
+type Props = {
+	navigation: ScreenNavigationProp;
+	route: ScreenRouteProp;
+} | BottomTabBarProps;
 
 export default interface IAiMobile {
-	data: { [key: string]: unknown; };	// !!! Do not modify this
-	readonly page: Component<NavigationInjectedProps>;
+	props: Props;
 	readonly local: { [key: string]: unknown; };
-	readonly global: { [key: string]: unknown; };
-	// eslint-disable-next-line semi
-}
-
-export interface IAi extends IAiMobile {
+	set_data<T>(key: string, value: T): void;
 	emit(mm: IAiMobile, event: string, ...args: unknown[]): Promise<unknown>;
+	// eslint-disable-next-line semi
 }
